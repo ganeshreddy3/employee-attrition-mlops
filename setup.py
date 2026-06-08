9979c72fc35e11778b7fa7d9b4f9527d83eb75c8
Ganesh <21054cs051@gmail.com>
2026-06-08 23:21:26 +0530

    implement project packaging and dependency management

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="employee_attrition_mlops",
+    version="0.0.1",
+    author="Ganesh Reddy",
+    packages=find_packages(),
+)
---
+++ test_exception.py
@@ -0,0 +1,13 @@
+import sys
+
+from src.exception import CustomException
+
+
+try:
+    a = 10
+    b = 0
+
+    print(a / b)
+
+except Exception as e:
+    raise CustomException(e, sys)
